312a52ae02bb1b02850a4e3996df8de3a1fee7b9
Isha Kashif <ishak@udel.edu>
2025-07-17 15:47:14 +0500

    added black and white image feature

--- photo_editor.py
+++ photo_editor.py
@@ -1,8 +1,12 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
-path - './unedited_images'
+path = './unedited_images'
 pathOut = './edited_images'
 
 for filename in os.listdir(path):
-    
+    img = Image.open(f"{path}/{filename}")
+    edit = img.filter(ImageFilter.SHARPEN).convert('L')
+
+    clean_name = os.path.splitext(filename)[0]
+    edit.save(f'{pathOut}/{clean_name}_edited.jpg')
